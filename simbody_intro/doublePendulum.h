6e8ae9ab562d277572994bb6dd659cae8bcb9698
aihoque2 <aihoque2@illinois.edu>
2020-08-01 03:59:26 -0500

    object oriented lezzgeddit

---
+++ simbody_intro/doublePendulum.h
@@ -0,0 +1,20 @@
+/*pendulum.h to create our double pendulum object*/
+
+#include <iostream>
+#include <stdio.h>
+#include "Simbody.h"
+using namespace SimTK;
+
+class doublePendulum{
+
+	public:
+		MobilizedBody::Pin pendulum1; //upper pendulum
+		MobilizedBody::Pin pendulum2;
+		
+		doublePuendulum(); //just the default constructor
+		
+
+
+		
+
+}
